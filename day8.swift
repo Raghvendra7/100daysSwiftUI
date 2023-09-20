a44e15076807ae16b987661c5bf8b541f3a61316
Raghvendra Shri <raghvendrashri@raghvendra.shri>
2023-09-20 23:31:14 +0530

    Day8

---
+++ day8.swift
@@ -0,0 +1,61 @@
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+enum PasswordError: Error {
+    case short, obvious
+}
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
+
+
+enum SQRTError{
+    case outofBound, noroot
+}
+
+func testSQRT(_ number: Int) throws -> Int  {
+    if number < 1 || number > 10_000 {
+        throw SQRTError.outofBound
+    }
+    for i in 1...100 {
+           if number == i * i {
+               let root = i
+               break
+           } else {
+               throw SQRTError.noroot
+           }
+       }
+       return root
+}
