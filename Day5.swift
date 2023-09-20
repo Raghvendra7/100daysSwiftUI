597c927655c6901d3e98c44aef9121dfabebfecd
Raghvendra Shri <raghvendrashri@raghvendra.shri>
2023-09-20 22:32:05 +0530

    Day5

---
+++ Day5.swift
@@ -0,0 +1,48 @@
+var username = ""
+if username.count == 0 {
+    username = "Anonymous"
+}
+if username.isEmpty {
+    username = "Anonymous"
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
+
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
